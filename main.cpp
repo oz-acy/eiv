d706cde044e5088967e515bfb2414019a35516fc
oZ/acy <acy@hiemalis.org>
2021-11-23 14:10:19 +0900

    modify

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
  *  @date 2019.4.24  修正 (v0.38) 巡覽順指定にかかる擴張
  *  @date 2019.8.30  修正 exceptionのcatchの仕方を修正。uraniaに追從。
  *  @date 2021.3.23  修正 (v0.39) 表示モード追加
+ *  @date 2021.4.4   修正 liburaniaの仕樣變更に追從
  *
  */
 #include "eiv.h"
@@ -40,21 +41,21 @@ int WINAPI wWinMain(HINSTANCE hi, HINSTANCE, LPWSTR, int)
     wf.minbox = true;
     wf.maxbox = true;
     wf.drag_and_drop = true;
-    wf.w = 300;
-    wf.h = 280;
+    wf.w = 400;
+    wf.h = 350;
 
     auto hndlr = std::make_unique<EIVWMHandler>();
-    hndlr->regist(EIV_MENU_OPEN, EIViewer::onMenuOpen);
-    hndlr->regist(EIV_MENU_END, EIViewer::onMenuEnd);
-    hndlr->regist(EIV_MENU_ABOUT, EIViewer::onMenuAbout);
-    hndlr->regist(EIV_MENU_SAVE, EIViewer::onMenuSave);
-    hndlr->regist(EIV_MENU_CNV_TO256, EIViewer::onMenuCnvTo256);
-    hndlr->regist(EIV_MENU_CNV_GS, EIViewer::onMenuCnvGS);
-    hndlr->regist(EIV_MENU_WALLPAPER, EIViewer::onMenuWallpaper);
-    hndlr->regist(EIV_MENU_SORTBYNAME, EIViewer::onMenuSortByName);
-    hndlr->regist(EIV_MENU_SORTBYTIME, EIViewer::onMenuSortByTime);
-    hndlr->regist(EIV_MENU_ACTUAL_SIZE, EIViewer::onMenuActualSize);
-    hndlr->regist(EIV_MENU_SCALING, EIViewer::onMenuScaling);
+    hndlr->setCommand(EIV_MENU_OPEN, EIViewer::onMenuOpen);
+    hndlr->setCommand(EIV_MENU_END, EIViewer::onMenuEnd);
+    hndlr->setCommand(EIV_MENU_ABOUT, EIViewer::onMenuAbout);
+    hndlr->setCommand(EIV_MENU_SAVE, EIViewer::onMenuSave);
+    hndlr->setCommand(EIV_MENU_CNV_TO256, EIViewer::onMenuCnvTo256);
+    hndlr->setCommand(EIV_MENU_CNV_GS, EIViewer::onMenuCnvGS);
+    hndlr->setCommand(EIV_MENU_WALLPAPER, EIViewer::onMenuWallpaper);
+    hndlr->setCommand(EIV_MENU_SORTBYNAME, EIViewer::onMenuSortByName);
+    hndlr->setCommand(EIV_MENU_SORTBYTIME, EIViewer::onMenuSortByTime);
+    hndlr->setCommand(EIV_MENU_ACTUAL_SIZE, EIViewer::onMenuActualSize);
+    hndlr->setCommand(EIV_MENU_SCALING, EIViewer::onMenuScaling);
 
     auto win = wf.create(std::move(hndlr), EIV_MAIN_MENU);
 
@@ -65,15 +66,14 @@ int WINAPI wWinMain(HINSTANCE hi, HINSTANCE, LPWSTR, int)
     eiv->handleMenu(win.get());
     eiv->sizeHandleAndMore(win.get());
 
-    System::messageLoop();
+    int ret = System::messageLoop();
 
-
-    return 0;
+    return ret;
   }
   catch(themis::EmptyException& exc)
   {
     System::alert(L"error", L"Error");
-    return 1;
+    return -1;
   }
 }
 
